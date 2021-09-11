9df3ffa7378de4c1ad4da330d1897d436ecfcf4e
ehWld <729psw@gmail.com>
2021-09-12 01:32:22 +0900

    [Settings] Create templates

---
+++ 2021/template.cpp
@@ -0,0 +1,29 @@
+//  template.cpp
+//  Copyright © 2021 도찡. All rights reserved.
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#define _USE_MATH_DEFINES
+
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+using pii = pair<int, int>;
+using lint = long long;
+
+/* [🦔] [💜] [✨] */
+
+int main() {
+#ifndef ONLINE_JUDGE
+#endif
+
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+
+    return 0;
+}
