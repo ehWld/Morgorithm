e24f23c32ef35974e7abbb93d3b303ac7927a353
ehWld <729psw@gmail.com>
2021-10-26 23:47:13 +0900

    [PS] day1024: 1 solve

--- 2021/🎃October/1026/9093.swift
+++ 2021/🎃October/1026/9093.swift
@@ -11,3 +11,15 @@ let INF = INT32_MAX
 let MOD = 1e9 + 7
 
 /*[😈][🔮][🦔]*/
+
+var T = readInt()[0]
+
+while T > 0 {
+    T -= 1
+    let input = readStr()
+    input.forEach { str in
+        print("\(str.reversed().reduce(""){ return String($0) + String($1)})", terminator: " ")
+    }
+    print("")
+}
+
