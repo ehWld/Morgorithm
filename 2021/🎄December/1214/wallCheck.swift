f6e8856fd6edd09fe1e51013449d863e5fdc77e0
ehWld <729psw@gmail.com>
2021-12-15 03:26:10 +0900

    [1214] 17281

---
+++ 2021/🎄December/1214/wallCheck.swift
@@ -0,0 +1,12 @@
+//
+//  wallCheck.swift
+//  2021
+//
+//  Created by Dozzing on 2021/12/15.
+//
+
+import Foundation
+
+func solution(_ n:Int, _ weak:[Int], _ dist:[Int]) -> Int {
+    return 0
+}
