8c26f2ced369d73a5535f9aec49814e37212aa64
ehWld <729psw@gmail.com>
2021-12-15 02:14:37 +0900

    [1214] 20055.swift

--- 2021/🎄December/1214/20055.swift
+++ 2021/🎄December/1214/20055.swift
@@ -15,21 +15,43 @@ let MOD = 1e9 + 7
 var N = 0
 var K = 0
 var belt = [Int]()
+var robots = [Bool]()
 
 func solve() {
-    var input = readInt()
+    let input = readInt()
     N = input[0]
     K = input[1]
-
-    input = readInt()
-    for i in input {
-        belt.append(i)
-    }
+    belt = readInt()
+    robots = [Bool](repeating: false, count: N)
 
     var count = 0
-    var ans = 1
-    while count < K {
+    var beltBase = 0
+    var robotBase = 0
+    var ans = 0
 
+    while count < K {
         ans += 1
+        beltBase = (beltBase + 2*N - 1) % (2 * N)
+        robotBase = (robotBase + N - 1) % N
+        robots[(robotBase + N - 1) % N] = false
+
+        for i in (0...N-2).reversed() {
+            if !robots[(robotBase + i) % N] { continue } // 현재 위치에 로봇이 없음
+            if robots[(robotBase + i + 1) % N] || belt[(beltBase + i + 1) % (2 * N)] < 1 { continue } // 로봇을 옮길수 없음
+
+            robots[(robotBase + i + 1) % N] = true
+            robots[(robotBase + i) % N] = false
+            belt[(beltBase + i + 1) % (2 * N)] -= 1
+            if belt[(beltBase + i + 1) % (2 * N)] == 0 { count += 1 }
+            robots[(robotBase + N - 1) % N] = false
+        }
+
+        if belt[beltBase] > 0 {
+            belt[beltBase] -= 1
+            robots[robotBase] = true
+            if belt[beltBase] == 0 { count += 1 }
+        }
     }
+
+    print(ans)
 }
